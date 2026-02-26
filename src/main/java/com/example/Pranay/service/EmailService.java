f091d3d094b04b70c698035b37f0749880944430
Pranay YaYa <kkpranay102@gmail.com>
2026-02-26 11:37:41 +0530

    Email service fixed

--- src/main/java/com/example/Pranay/service/EmailService.java
+++ src/main/java/com/example/Pranay/service/EmailService.java
@@ -2,36 +2,34 @@ package com.example.Pranay.service;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import org.springframework.mail.MailMessage;
-import org.springframework.mail.SimpleMailMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import static java.awt.SystemColor.text;
 @Service
+@RequiredArgsConstructor
 public class EmailService {
 
-    private JavaMailSender mailSender;
+    private final JavaMailSender mailSender;
 
-    // for simple mail
-    public String sendPlainTextMail(String to,String subject,String text){
-        SimpleMailMessage mailMessage=new SimpleMailMessage();
-        mailMessage.setTo(to);
-        mailMessage.setSubject(subject);
-        mailMessage.setText(text);
-        mailSender.send(mailMessage);
-        return "Plain text email sent Successfully ";
-    }
 
-    public String sendHtmlMail(String to,String subject,String htmlContent)
-            throws MessagingException {
-        MimeMessage mimeMessage=mailSender.createMimeMessage();
-        MimeMessageHelper helper= new MimeMessageHelper(mimeMessage,true,"UTF-8");
-        helper.setTo(to);
-        helper.setSubject(subject);
-        helper.setText(htmlContent,true);
-        mailSender.send(mimeMessage);
-        return "Html email sent successfully" ;
+
+    public void sendHtmlMail(String to, String subject, String htmlContent) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper =
+                    new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(htmlContent, true);
+
+            mailSender.send(message);
+
+        } catch (MessagingException e) {
+            throw new RuntimeException("Failed to send email", e);
+        }
     }
 }
